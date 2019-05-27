2c025aefedf3038d3d20b4e767fa5d791a3d13be
lonzohope <hopemutheu@gmail.com>
2019-05-27 09:25:53 +0300

    navabar issues

---
+++ reward/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404,HttpResponseRedirect
+
+# Create your views here.
+def index(request):
+
+   return render(request, 'index.html')
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/mr/Desktop/awwards/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
